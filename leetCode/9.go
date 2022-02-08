d80462fea4074d4d2a4030694ab709196b9a2d26
Den <dden_vlad@mail.ru>
2022-02-08 22:27:45 +0300

    leetCode 9 - palindrome - int converter to slice

--- leetCode/9.go
+++ leetCode/9.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 )
 
 /**
@@ -11,16 +10,21 @@ An integer is a palindrome when it reads the same backward as forward.
 For example, 121 is a palindrome while 123 is not.
 */
 func main() {
-	result := palindrome(1235321)
+	result := isPalindrome(1331)
 	fmt.Println(result)
 }
 
-func palindrome(x int) bool {
-	var str = strconv.Itoa(x)
-	var strLen = len(str)
+func isPalindrome(x int) bool {
+	if x < 0 {
+		return false
+	}
+	var intSlice = IntToSlice(int64(x), []int64{})
+	var strLen = len(intSlice)
 
-	for i := 0; i <= strLen/2; i++ {
-		if str[i] == str[strLen-1-i] {
+	for i := 0; i < strLen/2; i++ {
+		//fmt.Println(intSlice[i])
+		//fmt.Println(intSlice[strLen-1-i])
+		if intSlice[i] == intSlice[strLen-1-i] {
 			continue
 		}
 
@@ -28,4 +32,14 @@ func palindrome(x int) bool {
 	}
 
 	return true
+}
+
+func IntToSlice(n int64, sequence []int64) []int64 {
+	if n != 0 {
+		i := n % 10
+		// sequence = append(sequence, i) // reverse order output
+		sequence = append([]int64{i}, sequence...)
+		return IntToSlice(n/10, sequence)
+	}
+	return sequence
 }
